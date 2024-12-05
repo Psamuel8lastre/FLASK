9da35be3aabf28a28a7a3781a73eb4290c899457
samue lastre <122571446+Psamuel8lastre@users.noreply.github.com>
2024-12-05 18:44:08 -0500

    Delete 3.app_crud_juego directory

--- 4.app_crud_juego/bd.py
+++
@@ -1,7 +0,0 @@
-import pymysql
-
-def obtener_conexion():
-    return pymysql.connect(host='localhost',
-                           user='root',
-                           passwd='',
-                           db='app_crud_juego')
